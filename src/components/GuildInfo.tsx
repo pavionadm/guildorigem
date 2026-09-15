ddf0a3dc4b5e9c3995deaa832a89ff9e2cbb9748
pavionadm <pavionapp@proton.me>
2026-09-14 21:16:33 -0300

    Change image source from origem.png to origem.jpg

--- src/components/GuildInfo.tsx
+++ src/components/GuildInfo.tsx
@@ -50,7 +50,7 @@ export default function GuildInfo(): React.ReactElement {
 
  <div className="frame-gold relative aspect-[4/5] w-full overflow-hidden rounded-sm">
           <Image
-            src="/origem.png"
+            src="/origem.jpg"
             alt="Cenário de AdventureQuest Worlds"
             fill
             quality={60}
