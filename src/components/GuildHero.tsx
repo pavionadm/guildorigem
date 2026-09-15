ce67a5ca578f32d4c005fb5dc539c76674a81968
pavionadm <pavionapp@proton.me>
2026-09-14 21:25:01 -0300

    Update background image URL in GuildHero component

--- src/components/GuildHero.tsx
+++ src/components/GuildHero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const BG_URL =
-  "https://res.cloudinary.com/dpyhkwaw9/image/upload/v1789418601/274dac53-75f0-4f38-8367-60ac07a71a75_1_lpshyg.png";
+  "https://res.cloudinary.com/dpyhkwaw9/image/upload/v1789431851/origem_d1iycp.jpg";
 
 export default function GuildHero(): React.ReactElement {
   return (
