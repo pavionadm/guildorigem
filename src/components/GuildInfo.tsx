c149fdde24fb7069d6231d16d3458a2460965f25
pavionadm <pavionapp@proton.me>
2026-09-14 21:09:25 -0300

    Update GuildInfo.tsx

--- src/components/GuildInfo.tsx
+++ src/components/GuildInfo.tsx
@@ -50,7 +50,7 @@ export default function GuildInfo(): React.ReactElement {
 
  <div className="frame-gold relative aspect-[4/5] w-full overflow-hidden rounded-sm">
           <Image
-            src="/zrh.jpg"
+            src="/origem.png"
             alt="Cenário de AdventureQuest Worlds"
             fill
             quality={60}
